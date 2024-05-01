5842989b263371392a5f071d1ad2199cb19fbd9d
Vyacheslav Samokhvalov <94900884+qelio@users.noreply.github.com>
2024-05-01 16:54:03 +0500

    Delete server directory

--- server/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <QCoreApplication> // Подключение ядра приложений QT (для использования библиотек и типов данных QT)
-#include "mytcpserver.h"
-
-int main(int argc, char *argv[]) // argc, argv - параметры командной строки
-{
-    QCoreApplication a(argc, argv); // Создание терминала (окна), в котором будет все отображаться
-    MyTcpServer myserv;
-    return a.exec(); // Запуск консольного приложения на исполнения
-}
