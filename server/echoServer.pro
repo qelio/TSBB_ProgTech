deb212d7d7c1e63523aa661643a2e8862454b0c8
Vyacheslav Samokhvalov <94900884+qelio@users.noreply.github.com>
2024-05-09 18:15:44 +0300

    Fix code

---
+++ server/echoServer.pro
@@ -0,0 +1,39 @@
+QT -= gui # Сервер консольный, отключение графического интерфейса
+
+QT += network # Для работы с сетью
+
+QT += sql
+
+# Автоматически генерируемая конфигурация, задает используемый компилятор
+CONFIG += c++11 console
+CONFIG -= app_bundle
+
+# The following define makes your compiler emit warnings if you use
+# any Qt feature that has been marked deprecated (the exact warnings
+# depend on your compiler). Please consult the documentation of the
+# deprecated API in order to know how to port your code away from it.
+
+DEFINES += QT_DEPRECATED_WARNINGS # Игнорирование warnings (то есть, если есть warnings, то проект все равно запустится)
+
+# You can also make your code fail to compile if it uses deprecated APIs.
+# In order to do so, uncomment the following line.
+# You can also select to disable deprecated APIs only up to a certain version of Qt.
+#DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
+
+# Здесь необходимо написать все файлы .cpp (если добавляем файл вручную или переименовываем - необходимо внести изменения)
+SOURCES += \
+    database.cpp \
+    functionserver.cpp \
+    main.cpp \
+    mytcpserver.cpp
+
+# Default rules for deployment.
+qnx: target.path = /tmp/$${TARGET}/bin
+else: unix:!android: target.path = /opt/$${TARGET}/bin
+!isEmpty(target.path): INSTALLS += target
+
+# Здесь необходимо написать все заголовочные файлы .h (если добавляем файл вручную или переименовываем - необходимо внести изменения)
+HEADERS += \
+    database.h \
+    functionserver.h \
+    mytcpserver.h
---
+++ server/functionserver.cpp
@@ -0,0 +1,52 @@
+#include "functionserver.h"
+
+// Функция для авторизации пользователя (на данный момент заглушка)
+QByteArray authUser (QString login, QString password, long sockId) {
+    if (login == "user" && password == "test") {
+        return ("auth+&" + login + "\r\n").toUtf8();
+    }
+    else {
+        return "auth-\r\n";
+    }
+}
+
+// Функция для регистрации пользователя (на данный момент заглушка)
+QByteArray regUser (QString login, QString password, QString email) {
+    if (login == "user" && password == "test" && email == "test@mail.ru") {
+        return ("reg+&" + login + "\r\n").toUtf8();
+    }
+    else {
+        return "reg-\r\n";
+    }
+}
+
+// Функция для вывода статистики по определенному пользователю (на данный момент заглушка)
+QByteArray getMyStat (long sockId) {
+    return "stat\r\n";
+}
+
+// Функция для вывода статистики по всем пользователям (на данный момент заглушка)
+QByteArray getAllStat () {
+    return "stat\r\n";
+}
+
+
+// Функция для парсинга получаемой строки
+QByteArray mainParser (QString request, long sockId) {
+    // Разделение строки по разделителю "&"
+    QStringList command = request.split("&");
+    // Далее идет проверка команд, если команда не соответствует, то возвращаем "incrorrect"
+    // Метод trimmed() необходим для исключения из строки такие символы, как \r, \n (whitespace characters)
+    if (command[0] == "auth") {
+        return authUser(command[1], command[2].trimmed(), sockId);
+    }
+    else if (command[0] == "reg") {
+        return regUser(command[1], command[2], command[3].trimmed());
+    }
+    else if (command[0] == "stat") {
+        return getMyStat(sockId);
+    }
+    else {
+        return "incorrent\r\n";
+    }
+}
